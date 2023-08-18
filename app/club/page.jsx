4c86e65979780bf325249ec5531157f7b06872fc
Your Name <ndoye.mohamed95@yahoo.fr>
2023-08-18 16:30:24 +0200

    page css

--- app/club/page.jsx
+++ app/club/page.jsx
@@ -2,37 +2,84 @@ import React from "react";
 
 const MonClub = () => {
   return (
-    <div className="container">
-      <h1 className=" blocClub pt-2 text-center">
-        Sculptez Votre Succès : Équipements de Fitness Haut de Gamme
-      </h1>
-      <h2 className="text-center">Révolutionnez Votre Entraînement </h2>
-      <div className="container">
-        <div className="row text-center pt-4">
-          <div className="">
+    <>
+      <div className="container blocClub text-center">
+        <div className="row d-flex ">
+          <h1 className="pb-5">
+            {" "}
+            Cours en Plein Air : Fitness à la Lumière du Jour !
+          </h1>
+          <div className=" ">
             <img
-              className="img-fluid w-50 Salle1"
-              src="img/salle-grande.jpg"
-              alt="installation salle de sport"
+              src="img/groupe2.jpg"
+              alt="cours collectif"
+              className="img-fluid"
             />
           </div>
-        </div>
-
-        <div className="texteClub pt-5 ">
-          <h3 className="text-center">DES ESPACES UNIQUES</h3>
-          <p className=" text-justify lh-2   ">
-            Rappelez-vous, chaque petit pas compte. Chaque goutte de sueur est
-            une victoire. Chaque jour où vous vous poussez un peu plus loin,
-            vous créez une base solide pour un avenir plus sain et plus
-            épanouissant. Alors, êtes-vous prêt à vous engager dans ce voyage
-            excitant ? Relevez le défi, repoussez les limites et découvrez le
-            pouvoir qui sommeille en vous. Rejoignez-nous dans notre salle de
-            sport, où les rêves deviennent des réalités et où la persévérance
-            construit des destins.
+          <h2 className=" pt-5 ">Chers passionnés de fitness</h2>
+          <p className="ClubText pt-2  ">
+            <span className="textFitness"> Venez nous rejoindre </span> alors
+            que nous apportons l'enthousiasme et la vitalité de Mpower Gym en
+            plein air. Nos séances d'entraînement en plein air sont conçues pour
+            vous offrir une nouvelle perspective sur le fitness et vous
+            reconnecter avec la nature tout en transpirant et en vous amusant.{" "}
+            <br />
+            <br />
+            <span className="textFitness">Sous le ciel ouvert, </span> vous
+            serez guidé par nos instructeurs dévoués à travers des séquences
+            dynamiques d'exercices cardio, de renforcement musculaire et de
+            mouvementsfonctionnels.
+            <br /> L'herbe sous vos pieds et le soleil sur votre visage
+            ajouteront une dimension rafraîchissante à votre routine habituelle.
+            <br />
+            <br />
+            <span className="textFitness">
+              Que vous soyez un adepte du jogging
+            </span>
+            un amateur de HIIT ou simplement à la recherche d'une manière
+            amusante de rester actif, nos Cours en Plein Air sont adaptés à tous
+            les niveaux de forme physique. Peu importe votre expérience,
+            préparez-vous à vous sentir revitalisé et à vivre une expérience
+            d'entraînement mémorable.
+            <br />
+            <br />
+            <span className="textFitness">Apportez votre sourire </span>
+            : votre bouteille d'eau et votre motivation pour rejoindre la
+            communauté Mpower dans une aventure fitness qui vous emmènera hors
+            des sentiers battus. Les Cours en Plein Air sont également une
+            excellente occasion de rencontrer d'autres membres, de partager des
+            objectifs et de créer des souvenirs ensemble.
+            <br />
+            <br />
+            <span className="textFitness">
+              préparez-vous à vous entraîner{" "}
+            </span>{" "}
+            ans un environnement inspirant où le ciel est la limite. Faites un
+            pas vers le bien-être et rejoignez-nous pour des Cours en Plein Air
+            qui font vibrer le corps et l'esprit.
+            <br />
+            <br />
+            <span className="textFitness">
+              Offres spéciales d'inscription{" "}
+            </span>{" "}
+            Nous avons hâte de vous voir prendre de l'élan sous le soleil
+            radieux !
+            <br />
+            <br />
+            Avec enthousiasme, L'équipe Mpower Gym.{" "}
           </p>
         </div>
+        <br />
+        <br />
       </div>
-    </div>
+
+      {/* <div className="container">
+        <h2 className="text-center">Cours Collectif </h2>
+        <div className="row">
+          <div className="col-lg-6"></div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
